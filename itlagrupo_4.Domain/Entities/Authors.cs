84aea7ac95ce43cf4db32e13bf13ce1073f838ea
KerwinAngeles <kerwinangeles12@gmail.com>
2023-06-08 10:23:52 -0600

    agregue la clase author y sus atributos

---
+++ itlagrupo_4.Domain/Entities/Authors.cs
@@ -0,0 +1,18 @@
+﻿
+
+namespace itlagrupo_4.Domain.Entities
+{
+    public class Authors
+    {
+        public int au_id { get; set; }
+        public string? au_lname { get; set;}
+        public string? au_fname { get; set;}
+        public int phone { get; set;}
+        public string? address { get; set;}
+        public string? city { get; set;}
+        public string? state { get; set; }
+        public string? zip { get; set;}
+        public int contract { get; set;}   
+
+    }
+}
