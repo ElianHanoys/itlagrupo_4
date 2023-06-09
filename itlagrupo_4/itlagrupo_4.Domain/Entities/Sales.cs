fcf906e144441c06627d2e3a586938720bc952aa
Julio Noboa <julionoboa26@gmail.com>
2023-06-08 20:30:23 -0400

    Cambios realizados en entities

---
+++ itlagrupo_4/itlagrupo_4.Domain/Entities/Sales.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace itlagrupo_4.Domain.Entities
+{
+    public class Sales
+    {
+        public int stor_id { get; set; }
+
+        public int ord_num { get; set; }
+
+        public DateTime ord_date { get; set; }
+
+        public int qty { get; set; }
+
+        public String payterms { get; set; }
+
+        public String title_id { get; set; }
+
+    }
+}
