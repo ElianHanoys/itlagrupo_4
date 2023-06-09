30cd86a9d004921b161fb180995166dc67c32fa3
Andy Augusto Encarnacion Morel <20220995@itla.edu.do>
2023-06-08 13:09:17 -1200

    Agregando Employee

---
+++ itlagrupo_4/itlagrupo_4.Domain/Entities/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Class1
+{
+	public Employee()
+	{
+		public string emp_id {get; set; }
+	    public string fname { get; set;}
+		public string? minit {  get; set; }
+	    public string lname {get; set;}
+	    public int job_id { get; set; }
+		public int? job_lvl {get; set; }
+	    public string pub_id { get; set; }
+	    public Datetime hire_date { get; set; }
+
+	}
+}
