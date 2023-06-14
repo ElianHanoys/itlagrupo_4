84007841bbe3b3f7c9815cadd6035d211384e16d
ElianHanoys <elianhanoysrodriguezfeliz@gmail.com>
2023-06-13 22:07:17 -0400

    Actualizacion Publishers, Jobs 20221042

---
+++ itlagrupo_4/itlagrupo_4.Domain/Entities/jobs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itlagrupo_4.Domain.Entities
+{
+    public class jobs
+    {
+        public int job_id;
+        public string? job_desc;
+        public int min_lvl;
+        public int max_lvl;
+    }
+}
---
+++ itlagrupo_4/itlagrupo_4.Domain/Entities/publishers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace itlagrupo_4.Domain.Entities
+{
+    public class publishers
+    {
+        public string? pub_id;
+        public string? pub_name;
+        public string? city;
+        public string? state;
+        public string? country;
+    }
+}
