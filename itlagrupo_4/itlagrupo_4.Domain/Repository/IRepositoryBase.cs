a6fded4c63a04753086e7d1ae6574dbe0e2b7996
ameliahcross <ameliahcross@gmail.com>
2023-06-17 18:02:31 -0400

    methods

--- itlagrupo_4/itlagrupo_4.Domain/Repository/IRepositoryBase.cs
+++ itlagrupo_4/itlagrupo_4.Domain/Repository/IRepositoryBase.cs
@@ -1,14 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 
+
 namespace itlagrupo_4.Domain.Repository
 {
-	public interface IRepositoryBase<TEntity> where TEntity : class 
-	{
-		void save(TEntity entity);
+	public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        TEntity GetEntity(int titlesID);
+        List<TEntity> GetEntities();
+
+        void save(TEntity entity);
+        void Save(TEntity[] entities);
         void update(TEntity entity);
-		TEntity GetEntityById(int Id);
-		List<TEntity> GetEntities();
+        void Remove(TEntity entity);
+        TEntity GetEntityById(int Id);
+
     }
 }
 
--- itlagrupo_4/itlagrupo_4.Infrastructure/Repositories/TitlesRepository.cs
+++ itlagrupo_4/itlagrupo_4.Infrastructure/Repositories/TitlesRepository.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using itlagrupo_4.Domain.Entities;
 using itlagrupo_4.Domain.Repository;
+using itlagrupo_4.Infrastructure.Core;
+using itlagrupo_4.Infrastructure.Models;
+using Microsoft.Extensions.Logging;
 
 namespace itlagrupo_4.Infrastructure.Repositories
 {
-	public class TitlesRepository: IRepositoryBase<Titles>
-	{
+    public class TitlesRepository: BaseRepository<Titles>, IRepositoryBase<Titles>
+    {
+
+        private readonly ILogger<TitlesRepository> logger;
+        private readonly PubsContext context;
+
+        public TitlesRepository(ILogger<TitlesRepository> logger, PubsContext context) : base(context)
+        {
+            this.logger = logger;
+            this.context = context;
+        }
+
         public List<Titles> GetEntities()
 
         {   // adaptacion a nuestro requerimiento
@@ -21,15 +35,124 @@ namespace itlagrupo_4.Infrastructure.Repositories
             return title;
         }
 
-        public void save(Titles entity)
+        public override void Add(Titles entity)
         {
             // Adaptacion a nuestro requerimiento
+            if (this.Exists(cd => cd.titles == entity.titles))
+            {
+                throw new Exception("El título ya existe");
+            }
+            base.Add(entity);
+            base.SaveChanges();
         }
 
-        public void update(Titles entity)
+        public override void Update(Titles entity)
         {
-            // Adaptacion a nuestro requerimiento
+            try
+            {
+                Titles titleToUpdate = this.GetEntityById(entity.titlesID);
+
+                if (titleToUpdate is null)
+
+                    throw new Exception("El título no existe");
+
+                titleToUpdate.titlesID = entity.titlesID;
+                titleToUpdate.titles = entity.titles;
+                titleToUpdate.pubID = entity.pubID;
+                titleToUpdate.type = entity.type;
+                titleToUpdate.price = entity.price;
+                titleToUpdate.advance = entity.advance;
+                titleToUpdate.royalty = entity.royalty;
+                titleToUpdate.ytd_sales = entity.ytd_sales;
+                titleToUpdate.pubdate = entity.pubdate;
+
+                base.Update(titleToUpdate);
+                base.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError("Ocurrió un error actualizando el título", ex.ToString());
+            }
+            
         }
+
+        public override void Remove(Titles entity)
+        {
+            try
+            {
+                Titles titleToRemove = base.GetEntity(entity.titlesID);
+
+                if (titleToRemove is null)
+                    throw new Exception("El título no existe.");
+
+                titleToRemove.titles = entity.titles;
+
+                base.Update(titleToRemove);
+                base.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                this.logger.LogError("Ocurrió un error actualizando el título", ex.ToString());
+            }
+        }
+
+
+        public List<TitlesModel> GetTitles()
+        {
+
+            List<TitlesModel> titles = new List<TitlesModel>();
+            try
+            {
+                titles = (from tit in base.GetEntities()
+                           select new TitlesModel()
+                           {
+                               titlesID = tit.titlesID,
+                               titles = tit.titles,
+                               type = tit.type
+
+                           }).ToList();
+
+
+            }
+            catch (Exception ex)
+            {
+
+                this.logger.LogError($"Error obeteniendo los títulos: {ex.Message}", ex.ToString());
+            }
+
+            return titles;
+        }
+
+        public List<TitlesModel> GetTitlesByID(int titlesID)
+        {
+            List<TitlesModel> titles = new List<TitlesModel>();
+
+            try
+            {
+
+                this.logger.LogInformation($"Pase por aqui: {titlesID}");
+
+                titles = (from tit in base.GetEntities()
+                          join de in context.Titles.ToList() on tit.titlesID equals de.titlesID
+                          where tit.titlesID == titlesID
+                           && !tit.Deleted
+                          select new TitlesModel()
+                          {
+                              titlesID = tit.titlesID,
+                              titles = tit.titles,
+                              type = de.type,
+                          }).ToList();
+
+            }
+            catch (Exception ex)
+            {
+
+                this.logger.LogError($"Error obeteniendo los títulos: {ex.Message}", ex.ToString());
+            }
+
+            return titles;
+        }
+
     }
 
 }
