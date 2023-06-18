3dc61c47837b9d76113062a4599cf64f4a12428b
ameliahcross <ameliahcross@gmail.com>
2023-06-18 18:58:15 -0400

    API

--- itlagrupo_4/itlagrupo_4.Api/Program.cs
+++ itlagrupo_4/itlagrupo_4.Api/Program.cs
@@ -1,3 +1,12 @@
+using itlagrupo_4.Infrastructure.Context;
+using NuGet.Protocol.Core.Types;
+using Microsoft.EntityFrameworkCore;
+using itlagrupo_4.Infrastructure.Context;
+using itlagrupo_4.Infrastructure.Interfaces;
+using itlagrupo_4.Infrastructure.Repositories;
+using System.Runtime.CompilerServices;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//Registro de dependencia base de de datos //
+builder.Services.AddDbContext<itlagrupo_4Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("PubsContext")));
+
+// Repositories //
+builder.Services.AddTransient<ITitlesRepository, TitlesRepository>();
+
+// Registros de app services //
+builder.Services.AddTransient<ITitlesService, TitlesService>();
 
 var app = builder.Build();
 
